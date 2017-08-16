84f67cd96c1aa6d1dc400334e45838ef41ad5d1d
M1p0 <lx669196851@gmail.com>
2017-08-16 22:24:31 +0800

    ver0.1

---
+++ test/test.cpp
@@ -0,0 +1,49 @@
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+HHOOK keyboardHook = 0;
+
+//LRESULT CALLBACK HookCallback(int code, WPARAM wParam, LPARAM lParam)
+//{
+//    KBDLLHOOKSTRUCT *ks = (KBDLLHOOKSTRUCT*)lParam;
+//    if (ks->vkCode == 'A')
+//    {
+//        cout << "已拦截A键" << endl;
+//        return 1;
+//    }
+//    return CallNextHookEx(0, code, wParam, lParam);
+//}
+
+
+LRESULT CALLBACK HookCallback(int code, WPARAM wParam, LPARAM lParam)
+{
+    return 1;
+}
+
+
+int main()
+{
+    keyboardHook = SetWindowsHookExA(WH_KEYBOARD_LL, HookCallback, GetModuleHandleA(0), 0);
+
+    cout << "程序正常运行" << endl;
+    cout << "关闭程序以恢复键盘" << endl;
+
+    //不可漏掉消息处理，不然程序会卡死
+    MSG msg;
+    while (1)
+    {
+        if (PeekMessageA(&msg, 0, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        } 
+        else
+            Sleep(0);    //避免CPU全负载运行
+    }
+    UnhookWindowsHookEx(keyboardHook);
+
+    cout << "程序正常退出" << endl;
+
+    return 0;
+}
